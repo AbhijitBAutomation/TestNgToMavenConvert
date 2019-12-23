7b6d4089004c028cdcfb4b5f1d19657f5b0874a1
AbhijitBAutomation <abhijit.bhosale15@gmail.com>
2019-12-23 13:32:55 +0530

    First Commit

---
+++ src/com/jbk/LoginTestCase1.java
@@ -0,0 +1,12 @@
+package com.jbk;
+
+import org.testng.annotations.Test;
+
+public class LoginTestCase1 {
+  @Test
+  public void f1() {
+	  
+	  
+	  System.out.println("Hello1");
+  }
+}
---
+++ src/com/jbk/LoginTestCase2.java
@@ -0,0 +1,10 @@
+package com.jbk;
+
+import org.testng.annotations.Test;
+
+public class LoginTestCase2 {
+  @Test
+  public void f2() {
+	  System.out.println("Hello2");
+  }
+}
